b29a91163cba53237fcf289d1b1f071f772bea41
Augusto Baez <augustobaez@iMac-de-abc-desarrollo.local>
2023-02-01 16:43:20 -0300

    add post nuevo

--- src/components/AddAuthor.jsx
+++ src/components/AddAuthor.jsx
@@ -1,20 +1,39 @@
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const AddAuthor = () => {
   const [name, setName] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:8000/api/author/new`, {
+        name,
+      })
+      .then((res) => {
+        console.log(res, "succesful");
+        navigate("/main");
+      })
+      .catch((error) => {
+        console.log(error, "error haciendo post");
+      });
+  };
   return (
     <div>
       <h1>Favorite authors</h1>
       <p>
-        <a href="">Home</a>
+        <a href={`/main`}>Home</a>
       </p>
       <p>Edit this author:</p>
-      <div>
+      <form action="" onSubmit={handleSubmit}>
         <p>Name:</p>
-        <input type="text" />
-        <button>Cancel</button>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <button>
+          <a href={`/main`}></a>
+          Cancel
+        </button>
         <button>Submit</button>
-      </div>
+      </form>
     </div>
   );
 };
