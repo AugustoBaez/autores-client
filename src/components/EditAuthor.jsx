651b4bb8cc2919c1d7ef6787e02a40b2247379cb
Augusto Baez <augustobaez@iMac-de-abc-desarrollo.local>
2023-02-01 16:31:20 -0300

    update and delete

--- src/components/EditAuthor.jsx
+++ src/components/EditAuthor.jsx
@@ -1,7 +1,25 @@
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 const EditAuthor = () => {
   const [name, setName] = useState("");
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .put(`http://localhost:8000/api/author/update/${id}`, {
+        name,
+      })
+      .then((res) => {
+        console.log(res, "succesful");
+        navigate("/main");
+      })
+      .catch((error) => {
+        console.log(error, "error haciendo post");
+      });
+  };
+
   return (
     <div>
       <h1>Favorite authors</h1>
@@ -9,12 +27,12 @@ const EditAuthor = () => {
         <a href="">Home</a>
       </p>
       <p>Edit this author:</p>
-      <div>
+      <form action="" onSubmit={handleSubmit}>
         <p>Name:</p>
-        <input type="text" />
+        <input type="text" onChange={(e) => setName(e.target.value)} />
         <button>Cancel</button>
-        <button>Submit</button>
-      </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
